7df3932cfc6b84b5a557bc0893d45e9f0214b1db
karolkarp <karol0karp@gmail.com>
2019-07-01 00:36:35 +0200

    Add layout

---
+++ store/actions/common.js
@@ -0,0 +1,13 @@
+export function setPlay(play){
+	return{
+		type:'SET_PLAY',
+		play
+	};
+}
+
+export function setSliderPosition(position){
+	return{
+		type:'SET_SLIDER_POSITION',
+		position
+	};
+}
--- store/reducers/common.js
+++ store/reducers/common.js
@@ -1,10 +1,21 @@
 /* eslint-disable indent */
 const initialState = {
+	play: false,
+	sliderPosition:0,
 };
 
 export const common = (state = initialState, action) => {
 	switch (action.type) {
-	
+		case 'SET_PLAY':
+			return {
+				...state,
+				play: action.play,
+			};
+		case 'SET_SLIDER_POSITION':
+			return {
+				...state,
+				position: action.sliderPosition,
+			};
 		default:
 			return state;
 	}
